e4eaf388173f8ed51f2e188c14361217fe8a738b
seohongik <jericho8901@gmail.com>
2025-06-21 13:58:37 +0900

    로그인까지 구현 완료 다음 아이디 비밀번호 찾기

---
+++ src/main/java/com/project/bookommendbe/dto/LoginVO.java
@@ -0,0 +1,18 @@
+package com.project.bookommendbe.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class LoginVO {
+
+    @Email(message = "이메일을 입력해주세요")
+    @NotEmpty(message = "값을 입력해주세요")
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*()-+=]).{8,}$", message = "대문자,숫자,문자,특수문자 최소 8자 이상이어야 합니다")
+    private String password;
+
+}
