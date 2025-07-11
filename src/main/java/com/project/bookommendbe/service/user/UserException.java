ef92687c9a0219e9f1c701e62c663d0027abfbc6
seohongik <jericho8901@gmail.com>
2025-07-11 21:04:43 +0900

    중복회원 막기 및 회원가입 성공시 goBack메서드 적용

---
+++ src/main/java/com/project/bookommendbe/service/user/UserException.java
@@ -0,0 +1,13 @@
+package com.project.bookommendbe.service.user;
+
+public class UserException extends RuntimeException {
+
+    private final String message;
+
+
+    public UserException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+}
