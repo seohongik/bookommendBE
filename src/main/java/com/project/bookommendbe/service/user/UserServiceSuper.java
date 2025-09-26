df70742b5ed0b869e9c2f744898ad6e8e3f5aefe
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-09-26 09:30:39 +0900

    Update UserServiceSuper.java

--- src/main/java/com/project/bookommendbe/service/user/UserServiceSuper.java
+++ src/main/java/com/project/bookommendbe/service/user/UserServiceSuper.java
@@ -16,19 +16,19 @@ public abstract class UserServiceSuper {
 
     Optional<User> findUserByEmailAndPassword( String email, String password){
         return userRepository.findUserByEmailAndPassword(email, password);
-    };
+    }
 
     Optional<User> findUserBySignUpIdAndPhoneNumber( String signUpId, String phoneNumber){
         return userRepository.findUserBySignUpIdAndPhoneNumber(signUpId, phoneNumber);
-    };
+    }
 
     Optional<User> findUserByEmailAndPhoneNumber( String email, String phoneNumber){
         return userRepository.findUserByEmailAndPhoneNumber(email, phoneNumber);
-    };
+    }
 
     Optional<User> findUserByEmailAndPhoneNumberAndPasswordAuthNumber(String email, String phoneNumber, int authNumber){
         return userRepository.findUserByEmailAndPhoneNumberAndPasswordAuthNumber(email, phoneNumber, authNumber);
-    };
+    }
 
     public abstract Optional<User> getUserByIdOpen(Long id);
 
