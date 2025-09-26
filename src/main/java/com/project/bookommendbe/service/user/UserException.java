b05f1f66ce59ec0384a4d94478fa2dd9a6439417
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-09-26 09:24:49 +0900

    Update UserException.java

--- src/main/java/com/project/bookommendbe/service/user/UserException.java
+++ src/main/java/com/project/bookommendbe/service/user/UserException.java
@@ -1,13 +1,9 @@
 package com.project.bookommendbe.service.user;
 
 public class UserException extends RuntimeException {
-
     private final String message;
-
-
     public UserException(String message) {
         super(message);
         this.message = message;
     }
-
 }
