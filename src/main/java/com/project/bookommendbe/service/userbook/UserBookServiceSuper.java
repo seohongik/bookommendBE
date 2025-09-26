f1b8dd20e0d3101862fd75df9f35068176b30a55
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-09-26 09:42:14 +0900

    Update UserBookServiceSuper.java

--- src/main/java/com/project/bookommendbe/service/userbook/UserBookServiceSuper.java
+++ src/main/java/com/project/bookommendbe/service/userbook/UserBookServiceSuper.java
@@ -11,34 +11,23 @@ import java.util.Optional;
 public abstract class UserBookServiceSuper {
 
     protected UserBookRepository userBookRepository;
-
     public UserBookServiceSuper(UserBookRepository userBookRepository) {
         this.userBookRepository = userBookRepository;
     }
-
-
     List<UserBook> findUserBooksByUser(User user){
         return userBookRepository.findUserBooksByUser(user);
     };
-
     Optional<UserBook> findUserBookByIdAndUser(Long userBookId, User user){
         return userBookRepository.findUserBookByIdAndUser(userBookId, user);
     };
-
     Optional<UserBook> findUserBookByUserAndBookIsbn(User user, String bookIsbn){
         return userBookRepository.findUserBookByUserAndBookIsbn(user, bookIsbn);
     };
-
     boolean existsUserBookByUserAndBookIsbn(User userId, String bookIsbn){
         return userBookRepository.existsUserBookByUserAndBookIsbn(userId, bookIsbn);
     };
-
     public abstract List<UserBook> getUserBooksByUserOpen(Optional<User>  user);
-
-
     public abstract List<UserBook> findAllOpen();
-
     public abstract  Optional<UserBook> saveReadBookPageCountAndStatusOpen(User user, RecordAndReviewSaveVO saveRequest);
-
     public abstract void saveCategoryBookOpen(Book book);
 }
