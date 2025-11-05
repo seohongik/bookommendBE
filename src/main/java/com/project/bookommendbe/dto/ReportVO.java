96cf6c62e8b7c1afe11815f933eea7fcf75c4de8
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-11-05 10:07:45 +0900

    Add ReportVO class for report data structure

---
+++ src/main/java/com/project/bookommendbe/dto/ReportVO.java
@@ -0,0 +1,31 @@
+package com.project.bookommendbe.dto;
+
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Data
+public class ReportVO {
+
+    private Long userId;
+
+    private Long userBookId;
+
+    private String reportTitle;
+
+    private String reportContent;
+
+    private List<String> sympathyLines;
+
+    private LocalDateTime reportDateTime;
+
+    private LocalDateTime createdAt;
+}
