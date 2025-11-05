dc998c8eb1b11bd8de94484f4e260a891aa7bba8
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-11-05 10:23:33 +0900

    Add ReportController for user report handling

---
+++ src/main/java/com/project/bookommendbe/service/report/ReportController.java
@@ -0,0 +1,36 @@
+package com.project.bookommendbe.service.report;
+
+import com.project.bookommendbe.dto.RecordAndReviewSaveVO;
+import com.project.bookommendbe.dto.ReportVO;
+import com.project.bookommendbe.entity.Report;
+import com.project.bookommendbe.entity.User;
+import com.project.bookommendbe.entity.UserBook;
+import com.project.bookommendbe.service.recordAndReview.record.RecordService;
+import com.project.bookommendbe.service.recordAndReview.review.ReviewService;
+import com.project.bookommendbe.service.user.UserService;
+import com.project.bookommendbe.service.userbook.UserBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class ReportController {
+
+
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @PostMapping("/c1/userReport")
+    public void saveUserReportBy(@RequestBody ReportVO saveRequest) {
+
+        //TODO 컨트롤러 만들기
+
+    }
+}
